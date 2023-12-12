8779ec2fc0d6f37d903867ea1db622d986124f7e
Claudia <claui@users.noreply.github.com>
2023-12-12 19:40:30 +0100

    Use shuffle, not sample, for readability

--- gae_bolg/path.py
+++ gae_bolg/path.py
@@ -10,7 +10,9 @@ def draw(graph: nx.Graph, length: int, _random: Random = Random()) -> list[int]:
     """Return a random path of the given length in the given graph."""
 
     def random_paths() -> Iterator[list[int]]:
-        for start_node in _random.sample(graph.nodes, k=len(graph.nodes)):
+        nodes = list(graph.nodes)
+        _random.shuffle(nodes)
+        for start_node in nodes:
             yield from _random_paths_starting_at(graph, start_node, length, _random)
 
     if (path := next(random_paths(), None)) is None:
@@ -29,8 +31,9 @@ def _random_paths_starting_at(
     tail: nx.Graph = graph.copy()
     tail.remove_node(source)
     neighbors: list[int] = list(graph.neighbors(source))  # type: ignore
+    random.shuffle(neighbors)
     next_node: int
-    for next_node in random.sample(neighbors, k=len(neighbors)):
+    for next_node in neighbors:
         yield from (
             [source] + p
             for p in _random_paths_starting_at(tail, next_node, length - 1, random)
